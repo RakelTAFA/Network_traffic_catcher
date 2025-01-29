14d3fdd9a49acc5f919235dfd635d960ff86ab2f
= <=>
2025-01-29 21:49:46 +0100

    Capture de paquets, à explorer plus en détail pour comprendre fonctionnement

--- network_traffic_catcher/main.cpp
+++ network_traffic_catcher/main.cpp
@@ -5,18 +5,22 @@
 using namespace std;
 
 
+void packet_handler(u_char*, const struct pcap_pkthdr*, const u_char*);
+
+
 int main()
 {
 	pcap_if_t* all_devices = NULL;
 	char error_buffer[PCAP_ERRBUF_SIZE];
 	unsigned short int number_of_devices = 0;
 
+	// Search for all available devices
 	if (pcap_findalldevs_ex(PCAP_SRC_IF_STRING, nullptr, &all_devices, error_buffer) == -1)
 	{
-		printf("Error finding devices : %s", error_buffer);
-		exit(1);
+		printf("Error finding devices : %s\n", error_buffer);
+		return -1;
 	}
-	if (all_devices == NULL) exit(1);
+	if (all_devices == NULL) return -1;
 
 	printf("Listing all available devices...\n");
 
@@ -65,15 +69,54 @@ int main()
 
 	if (selected_device == NULL)
 	{
-		printf("Error while selecting device...");
-		exit(1);
+		printf("Error while selecting device...\n");
+		pcap_freealldevs(all_devices);
+		return -1;
 	}
 
 	printf("\nYou selected %s", selected_device->description);
 
-    pcap_open_live(selected_device->name, 4096, 0, 100, error_buffer);
+	pcap_t* capture;
+	if ((capture = pcap_open(selected_device->name,
+		65536,
+		0, // My PC doesn't support promiscuous mode
+		1000,
+		NULL,
+		error_buffer
+	)) == NULL)
+	{
+		printf("\nUnable to open the adapter. %s is not supported by Npcap\n", selected_device->name);
+		pcap_freealldevs(all_devices);
+		return -1;
+	}
+
+	printf("\nListening on %s...\n", selected_device->description);
+
+	pcap_loop(capture, 0, packet_handler, NULL);
+
+	pcap_close(capture);
 
 	pcap_freealldevs(all_devices);
 	return 0;
 }
 
+
+void packet_handler(u_char* param, const struct pcap_pkthdr* header, const u_char* pkt_data)
+{
+	struct tm ltime;
+	char timestr[16];
+	time_t local_tv_sec;
+
+	(VOID)(param);
+	(CHAR)(pkt_data);
+
+	cout << &pkt_data << endl;
+
+	/* convert the timestamp to readable format */
+	local_tv_sec = header->ts.tv_sec;
+	localtime_s(&ltime, &local_tv_sec);
+	strftime(timestr, sizeof timestr, "%H:%M:%S", &ltime);
+
+	printf("%s,%.6d len:%d\n",
+		timestr, header->ts.tv_usec, header->len);
+}
